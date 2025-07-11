e59347a0514900369ef962189f128595f68c7ff2
rodrilang <rodrigolang90mail.com>
2025-07-11 18:43:12 -0300

    refactor SurfaceType

--- src/main/java/dev/rodrilang/tennis_tournaments/enums/SurfaceType.java
+++ src/main/java/dev/rodrilang/tennis_tournaments/enums/SurfaceType.java
@@ -1,15 +1,10 @@
 package dev.rodrilang.tennis_tournaments.enums;
 
 public enum SurfaceType {
-    CARPET, CLAY, GRASS, HARD;
 
-    public String getDisplayName() {
-        return switch (this) {
-            case CARPET -> "Carpeta";
-            case CLAY -> "Arcilla";
-            case GRASS -> "Césped";
-            case HARD -> "Cemento";
-        };
-    }
+    CARPET,
+    CLAY,
+    GRASS,
+    HARD
 }
 
